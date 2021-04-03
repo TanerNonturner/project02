e49af4cab3ee5f2db52c1e475b15aa07b814dbb2
TanerNonturner <tanerdonmez2017@gmail.com>
2021-04-03 23:03:07 +0300

    third message

--- src/FirstPackage/Test01.java
+++ src/FirstPackage/Test01.java
@@ -5,6 +5,7 @@ public class Test01 {
 	public static void main(String[] args) {
 		System.out.println("message from local master");
 		System.out.println("Second message from local master");
+		System.out.println("third message from local master");
 
 	}
 
