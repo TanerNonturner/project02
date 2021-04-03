263d0c1940d80024eb8afd232950620ea1c2d438
TanerNonturner <tanerdonmez2017@gmail.com>
2021-04-03 20:33:32 +0300

    second update

--- src/FirstPackage/Test01.java
+++ src/FirstPackage/Test01.java
@@ -4,6 +4,7 @@ public class Test01 {
 
 	public static void main(String[] args) {
 		System.out.println("message from local master");
+		System.out.println("Second message from local master");
 
 	}
 
